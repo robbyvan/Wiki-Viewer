867ed83c4664a8822de5eb428439e1790800285b
robbyvan <fzpinus@gmail.com>
2016-10-25 14:56:52 -0700

    object string JSON test

--- wiki.js
+++ wiki.js
@@ -5,6 +5,29 @@ $(document).ready(function(){
   });//open a new tab when clicked
 
   $(".searchbutton").on("click", function(event){
-    console.log($(".tosearch").serializeArray());
+    var fields = $(".tosearch").serializeArray();
+
+    console.log(fields);
+    //returns an object array
+    var jsonstr = JSON.stringify(fields);
+    console.log(jsonstr);
+    //object2string
+    console.log(JSON.parse(jsonstr));
+    //string2object
+
+    var o = {};
+
+    $(fields).each(function(index, element){
+      if (o[element.name] !== undefined) {
+        if (!o[element.name].push) {
+          o[element.name] = [o[element.name]];
+        }
+      }else{
+        o[element.name] = element.value || "";
+      }
+    });
+
+    console.log(o);//json2 JS object
+
   });
 });
