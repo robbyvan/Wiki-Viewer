91520f371df2f35d515aca498ad4d2d2066d0805
robbyvan <fzpinus@gmail.com>
2016-10-25 23:58:45 -0700

    try to display them all

--- wiki.js
+++ wiki.js
@@ -19,11 +19,21 @@ $(document).ready(function(){
       success: function(r) {
         console.log("request succeeded");
         console.log(r.query);
+        displayResults(r);
       },
       error: function(e) {
         console.log(e);
       }
     });
-
   });
 });
+
+function displayResults(data) {
+  var list = $("<ul name='results'></ul>");
+  $(data).each(function(key, element){
+    var linkElem = $("<a name='" + key +"' href='http://en.wikipedia.org/?curid=" + key + "'></a>");
+    $("<h1>" + element.title + "</h1><p>" + element.extract + "</p>").appendTo(linkElem);
+    $(linkElem).appendTo(list);
+  });
+  $(list).appendTo(".displayZone");
+}
