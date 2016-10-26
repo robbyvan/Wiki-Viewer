c1b25ee3ff958f389cb5051eeef59eb18beb61c3
robbyvan <fzpinus@gmail.com>
2016-10-26 00:24:31 -0700

    open a new tab for all a tag

--- wiki.js
+++ wiki.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".roam").on("click", function(){
+  $("a").on("click", function(){
     event.preventDefault();
     window.open($(this).attr("href"));//open a new tab when clicked
   });
