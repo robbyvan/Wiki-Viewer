67754abdeb3d2253995f4097e582716414d8a5f6
robbyvan <fzpinus@gmail.com>
2016-10-26 01:20:24 -0700

    typo

--- wiki.js
+++ wiki.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   });
 
   $(".discover").on("click", function(event){
-    $(".displayzones").empty();
+    $(".displayzone").empty();
     var fields = $(".searcharea").serializeArray();
     var title = fields[0].value;
     // Using jQuery
