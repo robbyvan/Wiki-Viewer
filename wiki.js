475eb3ee241d0714d4c3a371859d0ddb86858aeb
robbyvan <fzpinus@gmail.com>
2016-10-25 21:24:43 -0700

    tried jsonp
    still can not get data, y?

--- wiki.js
+++ wiki.js
@@ -8,16 +8,17 @@ $(document).ready(function(){
     var fields = $(".tosearch").serializeArray();
     var title = fields[0].value;
     // Using jQuery
+
     $.ajax({
-      url: "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch='" + title + '&callback=JSON_CALLBACK',
-      dataType: 'json',
-      type: 'GET',
-      headers: { 'Api-User-Agent': 'Example/1.0' },
-      success: function(data) {
-         console.log(data);
+      url: "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=" + title +"&format=json&exintro=1",
+      type: "GET",
+      dataType: "json",
+      jsonp: "jsonpCallback",
+      success: function(r) {
+        console.log(r);
       },
-      error: function(err){
-        console.log(err);
+      error: function(e) {
+        console.log(e);
       }
     });
 
