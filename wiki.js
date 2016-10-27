228c1d4593909f5355ca40b44f0c7501b3c6bca3
robbyvan <fzpinus@gmail.com>
2016-10-26 18:15:54 -0700

    updated css & auto scroll down

--- wiki.js
+++ wiki.js
@@ -1,40 +1,48 @@
 $(document).ready(function(){
-  $("a").on("click", function(){
+  $(".roam").on("click", function(){
     event.preventDefault();
     window.open($(this).attr("href"));//open a new tab when clicked
   });
 
   $(".discover").on("click", function(event){
+    scrollDown(event);
     $(".displayzone").empty();
     var fields = $(".searcharea").serializeArray();
     var title = fields[0].value;
-    // Using jQuery
-
+    // Use JSONP to get data
     $.ajax({
       url: 'http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + title,
       type: "GET",
       dataType: "jsonp",
       headers: { 'Api-User-Agent': 'Example/1.0' },
       jsonp: "callback",
-      jsonpCallback:"flightHandler",
+      jsonpCallback:"?",
       success: function(r) {
         console.log("request succeeded");
         console.log(r.query);
         displayResults(r.query.pages);//should pass r.query.pages instead of r or r.query
+        scrollDown();
       },
       error: function(e) {
-        console.log(e);
+        $("<p class='errmsg'>Sorry, an error occurs.</p>")
       }
     });
   });
 });
 
 function displayResults(data) {
-  var list = $("<ul name='results'></ul>");
+  var list = $("<ul class='results'></ul>");
   $.each(data, function(key, element){
     var linkElem = $("<a name='" + key +"' href='http://en.wikipedia.org/?curid=" + key + "'></a>");
     $("<li><h1>" + element.title + "</h1><p>" + element.extract + "</p></li>").appendTo(linkElem);
     $(linkElem).appendTo(list);
   });
   $(list).appendTo(".displayzone");
+}
+
+function scrollDown(event){
+  event.preventDefault();
+
+  $("html body").animate({scrollTop: $("#displayzone").offset().top}, 1000);
+  return false;
 }
